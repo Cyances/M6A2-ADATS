1acf0247dcfff591547a1ae1e6294273e315b443
Cyances <qpsibojos@gmail.com>
2024-05-13 23:01:14 +0800

    Update ADATS-1.3

--- M6A2Adats.cs
+++ M6A2Adats.cs
@@ -57,7 +57,7 @@ namespace M6A2Adats
         public static AmmoType ammo_m792;
         public static AmmoType ammo_I_TOW;
 
-        static MelonPreferences_Entry<bool> useGau, useM919, useM920, adatsTandem, superOptics, betterDynamics, betterAI, compositeTurret, compositeHull, rotateAzimuth;
+        static MelonPreferences_Entry<bool> useGau, useM919, useM920, adatsTandem, superOptics, betterDynamics, betterAI, compositeTurret, compositeHull, rotateAzimuth, stabilityControl, rippleFire;
         static MelonPreferences_Entry<int> apCount, heCount;
         public static MelonPreferences_Entry<float> proxyDistance;
 
@@ -65,25 +65,41 @@ namespace M6A2Adats
         {
             useGau = cfg.CreateEntry<bool>("GAU12", true);
             useGau.Description = "Replaces M242 (500 RPM) with GAU-12 (3600 RPM)";
+
             useM919 = cfg.CreateEntry<bool>("M919", false);
             useM919.Description = "Replaces M792 with M919 APFSDS";
+
             useM920 = cfg.CreateEntry<bool>("MPAB", false);
             useM920.Description = "Replaces APEX with MPAB (TD airburst)";
+
             apCount = cfg.CreateEntry<int>("APCount", 300);
             apCount.Description = "Round type count, give at least 1 per type (max of 1500)";
             heCount = cfg.CreateEntry<int>("HECount", 1200);
+
             adatsTandem = cfg.CreateEntry<bool>("ADATSTandem", false);
             adatsTandem.Description = "Better ERA defeat for ADATS";
+
             proxyDistance = cfg.CreateEntry<float>("ProxyDistance", 3);
             proxyDistance.Description = "Trigger distance of ADATS proximity fuze (in meters).";
+
+            rippleFire = cfg.CreateEntry<bool>("RippleFire", false);
+            rippleFire.Description = "Fire and guide multiple ADATS at the same time";
+
             rotateAzimuth = cfg.CreateEntry<bool>("RotateAzimuth", false);
             rotateAzimuth.Description = "Horizontal reticle stabilization when leading";
+
             superOptics = cfg.CreateEntry<bool>("SuperOptics", false);
             superOptics.Description = "More zoom levels/clearer image for main and thermal sights";
+
             betterDynamics = cfg.CreateEntry<bool>("BetterDynamics", false);
             betterDynamics.Description = "Better engine/transmission/suspension/tracks";
+
+            stabilityControl = cfg.CreateEntry<bool>("StabilityControl", false);
+            stabilityControl.Description = "Makes the tank more controllable at higher speeds";
+
             betterAI = cfg.CreateEntry<bool>("BetterAI", false);
             betterAI.Description = "Better AI spotting and gunnery";
+
             compositeHull = cfg.CreateEntry<bool>("CompositeHull", false);
             compositeHull.Description = "50% better protection with no weight penalty";
             compositeTurret = cfg.CreateEntry<bool>("CompositeTurret", false);
@@ -335,7 +351,7 @@ namespace M6A2Adats
                 //US Vehicles/M2 Bradley/FCS and sights
                 mainGun.FCS.SuperleadWeapon = true;
                 mainGun.FCS.SuperelevateWeapon = true;
-                mainGun.FCS.RegisteredRangeLimits = new Vector2(200, 4000);
+                mainGun.FCS.RegisteredRangeLimits = new Vector2(200, 6000);
                 mainGun.FCS.RecordTraverseRateBuffer = true;
                 mainGun.FCS.TraverseBufferSeconds = 0.5f;
 
@@ -354,7 +370,7 @@ namespace M6A2Adats
                 towGun.MaxSpeedToFire = 999f;
                 towGun.MaxSpeedToDeploy = 999f;
                 towGun.RecoilBlurMultiplier = 0.2f;
-                towGun.FireWhileGuidingMissile = true;
+                towGun.FireWhileGuidingMissile = rippleFire.Value;
                 vic.AimablePlatforms[2].ForcedStowSpeed = 999f;
 
 
@@ -431,23 +447,23 @@ namespace M6A2Adats
                 }
 
                 //Vehicle dynamics under testing
-                VehicleController m6a2Vc = vic_go.GetComponent<VehicleController>();
-                NwhChassis m6a2Chassis = vic_go.GetComponent<NwhChassis>();
-                UnitAI m6a2Ai = vic.GetComponentInChildren<UnitAI>();
-                DriverAIController m6a2dAic = vic.GetComponent<DriverAIController>();
+                VehicleController vicVC = vic_go.GetComponent<VehicleController>();
+                NwhChassis vicNwhC = vic_go.GetComponent<NwhChassis>();
+                UnitAI vicUAI = vic.GetComponentInChildren<UnitAI>();
+                DriverAIController vicDAIC = vic.GetComponent<DriverAIController>();
 
                 if (betterDynamics.Value)
                 {
-                    m6a2dAic.maxSpeed = 32;//20
+                    vicDAIC.maxSpeed = 32;//20
 
-                    m6a2Vc.engine.maxPower = 1200f;//530;
-                    m6a2Vc.engine.maxRPM = 4500f;//4000 ;
-                    m6a2Vc.engine.maxRpmChange = 3000f;//2000;
+                    vicVC.engine.maxPower = 1200f;//530;
+                    vicVC.engine.maxRPM = 4500f;//4000 ;
+                    vicVC.engine.maxRpmChange = 3000f;//2000;
 
-                    m6a2Vc.brakes.maxTorque = 55590;//49590
+                    vicVC.brakes.maxTorque = 55590;//49590
 
-                    m6a2Chassis._maxForwardSpeed = 32f;//16.4
-                    m6a2Chassis._maxReverseSpeed = 16f;//4.47
+                    vicNwhC._maxForwardSpeed = 32f;//16.4
+                    vicNwhC._maxReverseSpeed = 16f;//4.47
 
 
                     List<float> fwGears = new List<float>();
@@ -475,33 +491,33 @@ namespace M6A2Adats
                     Gears.Add(1.36f);
                     Gears.Add(1.16f);
 
-                    m6a2Vc.transmission.forwardGears = fwGears;//5 2.4 1.9 1.6 1.4 1.2
-                    m6a2Vc.transmission.gearMultiplier = 9.918f;//9.918
-                    m6a2Vc.transmission.gears = Gears;//
-                    m6a2Vc.transmission.reverseGears = rvGears;//-8
-                    m6a2Vc.transmission.shiftDuration = 0.1f;//.309
-                    m6a2Vc.transmission.shiftDurationRandomness = 0f;//.2
-                    m6a2Vc.transmission.shiftPointRandomness = 0.05f;//.05
+                    vicVC.transmission.forwardGears = fwGears;//5 2.4 1.9 1.6 1.4 1.2
+                    vicVC.transmission.gearMultiplier = 9.918f;//9.918
+                    vicVC.transmission.gears = Gears;//
+                    vicVC.transmission.reverseGears = rvGears;//-8
+                    vicVC.transmission.initialShiftDuration = 0.1f;//.309
+                    vicVC.transmission.shiftDurationRandomness = 0f;//.2
+                    vicVC.transmission.shiftPointRandomness = 0.05f;//.05
 
 
                     for (int i = 0; i < 12; i++)
                     {
                         //m3a2Vc.wheels[i].wheelController.damper.force = 2.3036f;//2.3036
-                        m6a2Vc.wheels[i].wheelController.damper.maxForce = 6500;//6500
-                        m6a2Vc.wheels[i].wheelController.damper.unitBumpForce = 6500;//6500
-                        m6a2Vc.wheels[i].wheelController.damper.unitReboundForce = 9000;//9000
+                        vicVC.wheels[i].wheelController.damper.maxForce = 6500;//6500
+                        vicVC.wheels[i].wheelController.damper.unitBumpForce = 6500;//6500
+                        vicVC.wheels[i].wheelController.damper.unitReboundForce = 9000;//9000
 
                         //m3a2Vc.wheels[i].wheelController.spring.bottomOutForce = 0f;//0
-                        m6a2Vc.wheels[i].wheelController.spring.force = 24079.51f;//24079.51
-                        m6a2Vc.wheels[i].wheelController.spring.length = 0.32f;//0.2809
-                        m6a2Vc.wheels[i].wheelController.spring.maxForce = 100000;//100000
-                        m6a2Vc.wheels[i].wheelController.spring.maxLength = 0.58f;//0.48
+                        vicVC.wheels[i].wheelController.spring.force = 24079.51f;//24079.51
+                        vicVC.wheels[i].wheelController.spring.length = 0.32f;//0.2809
+                        vicVC.wheels[i].wheelController.spring.maxForce = 100000;//100000
+                        vicVC.wheels[i].wheelController.spring.maxLength = 0.58f;//0.48
 
-                        m6a2Vc.wheels[i].wheelController.fFriction.forceCoefficient = 1.25f;//1.2
-                        m6a2Vc.wheels[i].wheelController.fFriction.slipCoefficient = 1f;//1
+                        vicVC.wheels[i].wheelController.fFriction.forceCoefficient = 1.25f;//1.2
+                        vicVC.wheels[i].wheelController.fFriction.slipCoefficient = 1f;//1
 
-                        m6a2Vc.wheels[i].wheelController.sFriction.forceCoefficient = 0.85f;//0.8
-                        m6a2Vc.wheels[i].wheelController.sFriction.slipCoefficient = 1f;//1 
+                        vicVC.wheels[i].wheelController.sFriction.forceCoefficient = 0.85f;//0.8
+                        vicVC.wheels[i].wheelController.sFriction.slipCoefficient = 1f;//1 
                     }
 
                     vic.AimablePlatforms[0].SpeedPowered = 80;//60
@@ -513,28 +529,36 @@ namespace M6A2Adats
 
                 if (betterAI.Value)
                 {
-                    m6a2Ai.SpotTimeMaxDistance = 3500;
-                    m6a2Ai.TargetSensor._spotTimeMax = 3;
-                    m6a2Ai.TargetSensor._spotTimeMaxDistance = 500;
-                    m6a2Ai.TargetSensor._spotTimeMaxVelocity = 7f;
-                    m6a2Ai.TargetSensor._spotTimeMin = 1;
-                    m6a2Ai.TargetSensor._spotTimeMinDistance = 50;
-                    m6a2Ai.TargetSensor._targetCooldownTime = 1.5f;
+                    vicUAI.SpotTimeMaxDistance = 3500;
+                    vicUAI.TargetSensor._spotTimeMax = 3;
+                    vicUAI.TargetSensor._spotTimeMaxDistance = 500;
+                    vicUAI.TargetSensor._spotTimeMaxVelocity = 7f;
+                    vicUAI.TargetSensor._spotTimeMin = 1;
+                    vicUAI.TargetSensor._spotTimeMinDistance = 50;
+                    vicUAI.TargetSensor._targetCooldownTime = 1.5f;
 
-                    m6a2Ai.CommanderAI._identifyTargetDurationRange = new Vector2(1.5f, 2.5f);
-                    m6a2Ai.CommanderAI._sweepCommsCheckDuration = 4;
+                    vicUAI.CommanderAI._identifyTargetDurationRange = new Vector2(1.5f, 2.5f);
+                    vicUAI.CommanderAI._sweepCommsCheckDuration = 4;
 
 
-                    m6a2Ai.combatSpeedLimit = 25;
-                    m6a2Ai.firingSpeedLimit = 20;
+                    vicUAI.combatSpeedLimit = 25;
+                    vicUAI.firingSpeedLimit = 20;
 
                     //m2Ai.AccuracyModifiers.Angle._radius = 2.4f;
-                    m6a2Ai.AccuracyModifiers.Angle.MaxDistance = 1500;
-                    m6a2Ai.AccuracyModifiers.Angle.MaxRadius = 5f;
-                    m6a2Ai.AccuracyModifiers.Angle.MinRadius = 2f;
-                    m6a2Ai.AccuracyModifiers.Angle.IncreaseAccuracyPerShot = false;
+                    vicUAI.AccuracyModifiers.Angle.MaxDistance = 1500;
+                    vicUAI.AccuracyModifiers.Angle.MaxRadius = 5f;
+                    vicUAI.AccuracyModifiers.Angle.MinRadius = 2f;
+                    vicUAI.AccuracyModifiers.Angle.IncreaseAccuracyPerShot = false;
 
                     MelonLogger.Msg("Better AI loaded");
+                }
+
+
+                if (stabilityControl.Value)
+                {
+                    vicVC.drivingAssists.stability.active = true;
+                    vicVC.drivingAssists.stability.enabled = true;
+                    vicVC.drivingAssists.stability.intensity = 0.2f;
                 }
 
                 /*
@@ -599,12 +623,12 @@ namespace M6A2Adats
                 int heCapacity = heCount.Value;
                 MelonLogger.Msg("Total AP/HE Count: " + (apCapacity + heCapacity));
 
-                if ((apCapacity + heCapacity) > 1500)
+                /*if ((apCapacity + heCapacity) > 1500)
                 {
                     apCapacity = 300;
                     heCapacity = 1200;
                     MelonLogger.Msg("Invalid total AP/HE amount, defaulting to 300 AP/1200 HE");
-                }
+                }*/
 
                 // M791 APFSDS-T
 
